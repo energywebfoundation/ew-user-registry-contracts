7eee8b5ed91ee1efcb06b8514e79b05bae58bc22
Heiko Burkhardt <heiko@slock.it>
2019-01-09 10:46:26 +0100

    json fix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { UserLogic } from './wrappedContracts/UserLogic';
 export { UserContractLookup } from './wrappedContracts/UserContractLookup';
 export { migrateUserRegistryContracts } from './utils/migrateContracts';
 
-import * as UserDBJSON from '../../dist/contracts/UserDB.json';
-import * as UserLogicJSON from '../../dist/contracts/UserLogic.json';
-import * as UserContractLookupJSON from '../../dist/contracts/UserContractLookup.json';
+import UserDBJSON from '../../dist/contracts/UserDB.json';
+import UserLogicJSON from '../../dist/contracts/UserLogic.json';
+import UserContractLookupJSON from '../../dist/contracts/UserContractLookup.json';
 export { UserDBJSON, UserLogicJSON, UserContractLookupJSON };
